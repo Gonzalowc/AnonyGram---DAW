65ecf3e45b32de722fc08c17c4f7b2810252a333
Gonzalo Waack Carneado <44806755+Gonzalowc@users.noreply.github.com>
2022-05-17 01:29:36 +0200

    Delete src/angular/anony-gram/src directory

--- src/angular/anony-gram/src/app/shared/icons/icons.ts
+++
@@ -1,9 +0,0 @@
-import { IconDefinition } from '@fortawesome/fontawesome-svg-core';
-import { faIdBadge, faUserCog,faUserMinus } from '@fortawesome/free-solid-svg-icons';
-import {  } from '@fortawesome/free-regular-svg-icons';
-
-export const icons: IconDefinition[] = [
-    faIdBadge,
-    faUserCog,
-    faUserMinus
-];
--- src/angular/anony-gram/src/app/shared/models/usuarioModel.ts
+++
@@ -1,8 +0,0 @@
-export interface UsuarioCompleto {
-    id_usuario: number,
-    activo: boolean,
-    name: string,
-    rol: string,
-    usuario: string,
-    password: string,
-}
